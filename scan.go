082798fda6dfe90ea86a763bbc986f600646e2be
Matthew Rothenberg <mrothenberg@gmail.com>
2020-08-10 16:35:30 -0400

    perf: bounded concurrency of worker pool
    This results in a slight speedup on my particular workstation, but more
    importantly significantly greater consistency and less variance in
    results when benchmarking with hyperfine.

--- scan.go
+++ scan.go
@@ -3,6 +3,7 @@ package deepclean
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"sync"
 
 	"github.com/karrick/godirwalk"
@@ -37,22 +38,32 @@ func Scan(path string, targets []string) *Scanner {
 
 	go func() {
 		defer close(resultsChan)
+
+		// spawn worker pool to perform stating of matched target directories
+		matchedDirs := make(chan string)
 		var wg sync.WaitGroup
+		for i := 0; i < runtime.NumCPU(); i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for path := range matchedDirs {
+					if s, err := Stat(path); err == nil {
+						resultsChan <- Result{Path: path, Stats: s}
+					}
+				}
+			}()
+		}
+
+		// primary file system walk looking for target directories
 		scanner.err = godirwalk.Walk(path, &godirwalk.Options{
 			Unsorted: true,
 			Callback: func(path string, de *godirwalk.Dirent) error {
 				// Positives are directories matching any target string. For
-				// each match, spawn background goroutine to gather stats, but
+				// each match, send to the worker pool to gather stats, but
 				// skip further walking of the subdir in this primary scan
 				// thread.
 				if de.IsDir() && inTargets(targets, path) {
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						if s, err := Stat(path); err == nil {
-							resultsChan <- Result{Path: path, Stats: s}
-						}
-					}()
+					matchedDirs <- path
 					return filepath.SkipDir
 				}
 				return nil
@@ -63,6 +74,7 @@ func Scan(path string, targets []string) *Scanner {
 				return godirwalk.SkipNode
 			},
 		})
+		close(matchedDirs)
 		wg.Wait()
 	}()
 	return &scanner
